e429052ea8ba93f7dd8804949577e3b0382a8f8f
Kültigin <kultiginozturk.itu@gmail.com>
2025-07-01 22:16:11 +0300

    Integrate recurring lookup table - small fix

--- service_type_generator/bq_client.py
+++ service_type_generator/bq_client.py
@@ -1,4 +1,27 @@
+import os
 from google.cloud import bigquery
+from google.oauth2 import service_account
 
 def get_bq_client():
-    return bigquery.Client()
+    # Path to your service-account JSON key.
+    # By default GOOGLE_APPLICATION_CREDENTIALS points here,
+    # but you can hard-code a path if you prefer.
+    key_path = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS", "path/to/key.json")
+
+    # We need both BigQuery AND Drive scopes
+    scopes = [
+        "https://www.googleapis.com/auth/bigquery",
+        "https://www.googleapis.com/auth/drive",
+    ]
+
+    # Build credentials with those scopes
+    creds = service_account.Credentials.from_service_account_file(
+        key_path,
+        scopes=scopes,
+    )
+
+    # Return a client that carries both scopes
+    return bigquery.Client(
+        credentials=creds,
+        project=creds.project_id,
+    )
